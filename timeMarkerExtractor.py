10c6dc41dfdd056ab5555139badb4856abce03aa
seyyed sajjad mirzababaie <sajjad.mirzababaie@gmail.com>
2021-11-20 18:08:50 +0330

    Create timeMarkerExtractor.py

---
+++ timeMarkerExtractor.py
@@ -0,0 +1,10 @@
+def time_marker_extractor(input_sentence):
+    """
+    function should output list of strings, each item in list is a time marker present in the input sentence.
+    :param input_sentence:
+    :return:
+    """
+
+    # TODO: write time extractor function
+    
+    return []
