d921ef2f33125974e12d838c411461eeb3eac265
amir kargaran <kargaranamir@gmail.com>
2021-11-22 15:12:38 +0330

    add pattern

--- pattern_to_regex.py
+++ pattern_to_regex.py
@@ -20,8 +20,38 @@ def annotaion():
     return annotaion_dict
 
 
+def pattern_replace(pattern_str):
+    replace_dict = {"num": "{numbers}", "Num": "{numbers}", "DP": "{dayPart}", "Day": "{days}", "RD": "{relativeDays}", "Next": "{next}", "Prev": "{past}"}
+
+    for key, value in replace_dict.items():
+        pattern_str = pattern_str.replace(key, value)
+
+    pattern_str = "fr'\b(?:" + pattern_str + "?)"
+    return pattern_str
+
+
+def normlize_pattern(path):
+    pattern_marks = []
+    with open(path, 'r') as file:
+        pattern_marks = file.readlines()
+        pattern_marks = pattern_marks[1:] # first line is empty
+        pattern_marks = [x.rstrip() for x in pattern_marks] # remove \n
+        pattern_marks = [pattern_replace(x) for x in pattern_marks]
+
+    return pattern_marks
+
+def pattern():
+    pattern_list = []
+    main_path = 'pattern_test_time'
+    files = os.listdir(main_path)
+    for f in files:
+        pattern_list = pattern_list + normlize_pattern(f"{main_path}/{f}")
+
+    return pattern_list
+
 annotaion_dict = annotaion()
-
-
 s= re.findall(fr'\b(?:{annotaion_dict["daynumbers"]}?)\s', "glass watre 29 یک  دو سه بیست‌وسه لیوان ")
 print(s)
+
+pattern_list = pattern()
+print(pattern_list)
