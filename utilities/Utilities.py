677bdc10e8ef04f87df48bb1c2170cf0d8f262fc
hamid jahad <jahad.hamid@gmail.com>
2021-11-22 22:13:07 +0330

    day normalize added

--- utilities/Utilities.py
+++ utilities/Utilities.py
@@ -5,6 +5,7 @@ def custom_nomalize(sentenceInput):
     sentence = arabic_to_persian_number(sentenceInput)
     sentence = arabic_to_persian_character(sentence)
     normalized_date = DateNormalize(sentence)
+    normalized_day = day_normalize(normalized_date) 
 
 
 def DateNormalize(sentence):
@@ -17,18 +18,22 @@ def DateNormalize(sentence):
         ": ": ":",
         " :": ":"
     }
-    # match = sentence.replace("/ ", "/")
-    # match = match.replace(" /", "/")
-    # match = match.replace("- ", "-")
-    # match = match.replace(" -", "-")
-    # match = match.replace(" :", ":")
-    # match = match.replace(": ", ":")
-    
     return replacement(dic, sentence)
 
 def CustomTokenize():
 
     pass
+
+def day_normalize(sentence):
+    temp = temp.split()
+    lst = ['1', '2', '3', '4', '5',
+           '۱', '۲', '۳', '۴', '۵',
+           'یک', 'دو', 'سه', 'چهار', 'چار', 'پنج']
+    if "شنبه" in temp:
+        for x in range(len(temp)):
+            if temp[x] == "شنبه" and temp[x-1] in lst:
+                temp[x], temp[x-1] = temp[x-1]+temp[x], ""
+    return temp   
 
 def arabic_to_persian_number(number):
     dic = {
