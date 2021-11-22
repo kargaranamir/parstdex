28f9ed8a454cf92bf5d87b559d4e8b031516897e
hamid jahad <jahad.hamid@gmail.com>
2021-11-22 22:00:25 +0330

    date and day normalization added

--- utilities/Utilities.py
+++ utilities/Utilities.py
@@ -1,18 +1,64 @@
 from hazm import *
+import re
+
+def custom_nomalize(sentenceInput):
+    sentence = arabic_to_persian_number(sentenceInput)
+    sentence = arabic_to_persian_character(sentence)
+    normalized_date = DateNormalize(sentence)
+
 
 def DateNormalize(sentence):
     # match = re.search(r'(\d+\s+/\s+\d+\s+/\s+\d+)','The date is 11/ 12/98')
-    match = sentence.replace("/ ", "/")
-    match = match.replace(" /", "/")
-    match = match.replace("- ", "-")
-    match = match.replace(" -", "-")
-    match = match.replace(" :", ":")
-    match = match.replace(": ", ":")
+    dic = {
+        "/ ": "/",
+        " /": "/",
+        "- ": "-",
+        " -": "-",
+        ": ": ":",
+        " :": ":"
+    }
+    # match = sentence.replace("/ ", "/")
+    # match = match.replace(" /", "/")
+    # match = match.replace("- ", "-")
+    # match = match.replace(" -", "-")
+    # match = match.replace(" :", ":")
+    # match = match.replace(": ", ":")
     
-    normalizer = Normalizer()
-    normalizer.normalize(match)
-    return match
+    return replacement(dic, sentence)
 
 def CustomTokenize():
 
     pass
+
+def arabic_to_persian_number(number):
+    dic = {
+        '١': '۱',
+        '٢': '۲',
+        '٣': '۳',
+        '٤': '۴',
+        '٥': '۵',
+        '٦': '۶',
+        '٧': '۷',
+        '٨': '۸',
+        '٩': '۹',
+        '٠': '۰',
+        }
+    return replacement(dic, number)
+
+def arabic_to_persian_character(userInput):
+    dic = {
+        'ك': 'ک',
+        'دِ': 'د',
+        'بِ': 'ب',
+        'زِ': 'ز',
+        'ذِ': 'ذ',
+        'شِ': 'ش',
+        'سِ': 'س',
+        'ى': 'ی',
+        'ي': 'ی'
+        }
+    return replacement(dic, userInput)
+
+def replacement(dic, text):
+    pattern = "|".join(map(re.escape, dic.keys()))
+    return re.sub(pattern, lambda m: dic[m.group()], str(text))
