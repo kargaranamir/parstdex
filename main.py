c7ad21df9a804a2578f6ad16637b3115f9f37156
seyyed sajjad mirzababaie <sajjad.mirzababaie@gmail.com>
2022-02-10 19:41:05 +0330

    refactor function to class told by amir
    @kargaranamir @hamidjahad

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import glob
 import os
 import re
 
-from timeMarkerExtractor import time_marker_extractor
+from timeMarkerExtractor import MarkerExtractor
 
 
 def main():
     # read all test inputs in Test folder
     file_list = glob.glob(os.path.join(os.path.abspath(os.getcwd()), './Test/in/*.txt'))
     file_list.sort(key=lambda x: int(re.search('(input)([0-9]+)(.txt)', x).group(2)))
+    extractor = MarkerExtractor()
     for filename in file_list:
         print(filename)
         with open(filename, 'r', encoding="utf8") as infile:
@@ -17,7 +18,7 @@ def main():
             print("Original Sentence:\n", input_sentence)
             # time_marker_extractor will return normalized sentence and time and date markers and corresponding
             # value evaluation
-            normalized_sentence, res, res_date, res_time = time_marker_extractor(input_sentence)
+            normalized_sentence, res, res_date, res_time = extractor.time_marker_extractor(input_sentence)
 
             # Print results
             print("Normalized Sentence:\n", normalized_sentence)
--- timeMarkerExtractor.py
+++ timeMarkerExtractor.py
@@ -4,88 +4,97 @@ from utilities.Utilities import Normalizer, deleteSubMatches
 from utilities.word_to_value import ValueExtractor
 
 
-def time_marker_extractor(input_sentence):
-    """
-    function should output list of strings, each item in list is a time marker present in the input sentence.
-    :param input_sentence: input sentence
-    :return:
-    normalized_sentence: normalized sentence
-    res: all extracted markers
-    res_date: all extracted date values
-    res_time: all extracted time values
-    """
+class MarkerExtractor:
+    def __init__(self):
+        # Normalizer: manage spaces, converts numbers to en, converts alphabet to fa
+        self.normalizer = Normalizer()
+        # Patterns: patterns to regex generator
+        self.patterns = Patterns()
+        # ValueExtractor: value extractor from known time and date
+        self.extractor = ValueExtractor()
 
-    # Normalizer: manage spaces, converts numbers to en, converts alphabet to fa
-    normalizer = Normalizer()
-    # Patterns: patterns to regex generator
-    patterns = Patterns()
-    # ValueExtractor: value extractor from known time and date
-    extractor = ValueExtractor()
+    def time_marker_extractor(self, input_sentence):
+        """
+        function should output list of strings, each item in list is a time marker present in the input sentence.
+        :param input_sentence: input sentence
+        :return:
+        normalized_sentence: normalized sentence
+        res: all extracted markers
+        res_date: all extracted date values
+        res_time: all extracted time values
+        """
 
-    # apply normalizer on input sentence
-    normalized_sentence = normalizer.normalize_cumulative(input_sentence)
+        # Normalizer: manage spaces, converts numbers to en, converts alphabet to fa
+        normalizer = self.normalizer
+        # Patterns: patterns to regex generator
+        patterns = self.patterns
+        # ValueExtractor: value extractor from known time and date
+        extractor = self.extractor
 
-    # define data structures to compute and postprocess the extracted patterns
-    output_raw = {"Date": [], "Time": []}
-    output_extracted = {}
-    output_flatten: list = []
-    output_flatten_keys = []
-    post_output_flatten = []
-    post_output_flatten_keys = []
+        # apply normalizer on input sentence
+        normalized_sentence = normalizer.normalize_cumulative(input_sentence)
 
-    # data structures to store all extracted markers data-time values
-    res = []
-    res_date = []
-    res_time = []
+        # define data structures to compute and postprocess the extracted patterns
+        output_raw = {"Date": [], "Time": []}
+        output_extracted = {}
+        output_flatten: list = []
+        output_flatten_keys = []
+        post_output_flatten = []
+        post_output_flatten_keys = []
 
-    # write Regexes into pattern.txt file
-    with open('patterns.txt', 'w', encoding="utf-8") as f:
+        # data structures to store all extracted markers data-time values
+        res = []
+        res_date = []
+        res_time = []
+
+        # write Regexes into pattern.txt file
+        with open('patterns.txt', 'w', encoding="utf-8") as f:
+            for key in patterns.regexes.keys():
+                for regex_value in patterns.regexes[key]:
+                    f.write(regex_value)
+                    f.write('\n')
+
+        # add pattern keys to dictionaries and define a list structure for each key
+        for key in patterns.regexes.keys():
+            output_raw[key]: list = []
+            output_extracted[key]: list = []
+
+        # apply regexes on normalized sentence and store extracted markers in output_raw
         for key in patterns.regexes.keys():
             for regex_value in patterns.regexes[key]:
-                f.write(regex_value)
-                f.write('\n')
+                # apply regex
+                out = re.findall(fr'\b(?:{regex_value})', normalized_sentence)
+                # ignore empty markers
+                if len(out) > 0:
+                    # store extracted markers in output_raw
+                    output_raw[key] = output_raw[key] + out
 
-    # add pattern keys to dictionaries and define a list structure for each key
-    for key in patterns.regexes.keys():
-        output_raw[key]: list = []
-        output_extracted[key]: list = []
+        # process result: make flatten value-key lists from dictionary
+        for key in output_raw.keys():
+            output_raw[key] = list(set(output_raw[key]))
+            output_flatten = output_flatten + output_raw[key]
+            output_flatten_keys = output_flatten_keys + [key] * len(output_raw[key])
 
-    # apply regexes on normalized sentence and store extracted markers in output_raw
-    for key in patterns.regexes.keys():
-        for regex_value in patterns.regexes[key]:
-            # apply regex
-            out = re.findall(fr'\b(?:{regex_value})', normalized_sentence)
-            # ignore empty markers
-            if len(out) > 0:
-                # store extracted markers in output_raw
-                output_raw[key] = output_raw[key] + out
+        # make values unique while they have different keys
+        for key, value in zip(output_flatten_keys, output_flatten):
+            if not value in post_output_flatten:
+                post_output_flatten.append(value)
+                post_output_flatten_keys.append(key)
 
-    # process result: make flatten value-key lists from dictionary
-    for key in output_raw.keys():
-        output_raw[key] = list(set(output_raw[key]))
-        output_flatten = output_flatten + output_raw[key]
-        output_flatten_keys = output_flatten_keys + [key] * len(output_raw[key])
+        # store unique values in output_flatten and corresponding keys in output_flatten_keys
+        output_flatten = post_output_flatten
+        output_flatten_keys = post_output_flatten_keys
 
-    # make values unique while they have different keys
-    for key, value in zip(output_flatten_keys, output_flatten):
-        if not value in post_output_flatten:
-            post_output_flatten.append(value)
-            post_output_flatten_keys.append(key)
+        # delete extracted markers the cover each other and get the longest one
+        output_extracted = deleteSubMatches(output_flatten, output_flatten_keys, normalized_sentence)
+        # extract date values from all post process markers
+        if output_extracted.get('Date'):
+            res_date = [extractor.compute_date_value(p_date) for p_date in output_extracted['Date']]
+        # extract time values from all post process markers
+        if output_extracted.get('Time'):
+            res_time = [extractor.compute_time_value(p_time) for p_time in output_extracted['Time']]
+        # make all the extracted markers flatten into a one list
+        for p_output in output_extracted.values():
+            res += p_output
 
-    # store unique values in output_flatten and corresponding keys in output_flatten_keys
-    output_flatten = post_output_flatten
-    output_flatten_keys = post_output_flatten_keys
-
-    # delete extracted markers the cover each other and get the longest one
-    output_extracted = deleteSubMatches(output_flatten, output_flatten_keys, normalized_sentence)
-    # extract date values from all post process markers
-    if output_extracted.get('Date'):
-        res_date = [extractor.compute_date_value(p_date) for p_date in output_extracted['Date']]
-    # extract time values from all post process markers
-    if output_extracted.get('Time'):
-        res_time = [extractor.compute_time_value(p_time) for p_time in output_extracted['Time']]
-    # make all the extracted markers flatten into a one list
-    for p_output in output_extracted.values():
-        res += p_output
-
-    return normalized_sentence, res, res_date, res_time
+        return normalized_sentence, res, res_date, res_time
