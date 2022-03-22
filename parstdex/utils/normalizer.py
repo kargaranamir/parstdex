df330082f24daae05a29c5c203806adf0b95e17e
amir kargaran <kargaranamir@gmail.com>
2022-03-22 05:08:56 +0430

    no need to space normalizer
    add testcase for parenthesis

--- parstdex/utils/normalizer.py
+++ parstdex/utils/normalizer.py
@@ -5,9 +5,6 @@ class Normalizer:
     """
     Normalizer class is used to:
     - normalized arabic alphabet into persian alphabet(normalize_alphabet)
-    - normalize space - digit to word concatenation - comma and other spacial symbols
-    - normalize_annotation method is used to normalize annotation files in utilities/annotation folder
-    - normalize_cumulative method execute normalize alphabet and space consequently
     """
     ALPHABET_DICT = {
         'ك': 'ک',
@@ -21,24 +18,15 @@ class Normalizer:
         'ي': 'ی',
     }
 
-    FA_NUMBERS = "۰|۱|۲|۳|۴|۵|۶|۷|۸|۹"
-    EN_NUMBERS = "0|1|2|3|4|5|6|7|8|9"
-    Symbols = ":|/|-|\."
-    C_NUMBERS = FA_NUMBERS + "|" + EN_NUMBERS + "|" + Symbols
-
     def normalize_alphabet(self, text):
+        """
+        normalizes arabic alphabet into persian alphabet
+        :return:
+        :type text: str
+        """
         pattern = "|".join(map(re.escape, self.ALPHABET_DICT.keys()))
         return re.sub(pattern, lambda m: self.ALPHABET_DICT[m.group()], str(text))
 
-    @staticmethod
-    def normalize_space(text):
-        # Remove persian comma
-        res = text.replace('،', ' ')
-        # Remove parenthesis
-        res = re.sub(r"\(|\)", " ", res)
-        return res
-
-    def normalize_cumulative(self, text):
+    def normalize_cumulative(self, text: str) -> str:
         res = self.normalize_alphabet(text)
-        res = self.normalize_space(res)
         return res
