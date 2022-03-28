845a74540c4ec2d1bf7500a06af61abf904e7afb
amir kargaran <kargaranamir@gmail.com>
2022-03-28 20:47:05 +0430

    v1.0.9

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md") as fh:
 
 setuptools.setup(
     name='parstdex',
-    version="1.0.8",
+    version="1.0.9",
     description="Persian time and date marker extractor",
     long_description=long_description,
     long_description_content_type="text/markdown",
