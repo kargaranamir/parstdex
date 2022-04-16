0c89ded7337db6c40641259bff5b0f0d2498bb53
Amir Hossein Kargaran <kargaranamir@gmail.com>
2022-04-16 19:16:07 +0430

    fix tokenizer
    support punct.

--- parstdex/utils/tokenizer.py
+++ parstdex/utils/tokenizer.py
@@ -1,4 +1,7 @@
+import re
+
+# persian-english word tokenizer
 def tokenize_words(text):
-    token_list = text.strip().split()
+    token_list = re.findall(r"[\w\u200c']+|[!\"#$%&\'()*+,-./:؛؟،;<=>?@[\\\]^_`{|}~]", text)
     token_list = [x.strip("\u200c") for x in token_list if len(x.strip("\u200c")) != 0]
     return token_list
