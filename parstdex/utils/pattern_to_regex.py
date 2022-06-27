35d491c2d5e8c3a4dc1c3e3d72d35c82aa8c5311
Amir Hossein Kargaran <kargaranamir@gmail.com>
2022-06-27 13:28:27 +0300

    Make compatible the merged code
    Co-Authored-By: Seyed Sajad Mirzababaei <sajjad.mirzababaie@gmail.com>

--- parstdex/utils/pattern_to_regex.py
+++ parstdex/utils/pattern_to_regex.py
@@ -117,6 +117,8 @@ class Patterns:
             special_words = get_special_words()
             self.patterns_path = os.path.join(os.path.dirname(__file__), 'pattern', "")
             self.cumulative_annotations = {**annotations.annotations_dict, **special_words}
+            for key, value in self.cumulative_annotations.items():
+                self.cumulative_annotations[key] = value.replace("<>", r'(?:[\\s\\u200c]{0,3})').replace(" ", r'(?:[\\u200c\\s]{1,3})')
             self.cumulative_annotations_keys = sorted(self.cumulative_annotations, key=len, reverse=True)
             files = os.listdir(self.patterns_path)
             for f in files:
